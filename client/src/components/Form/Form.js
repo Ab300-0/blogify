4d90772b7a4f46087e57c2ec41c176b9048e8fab
Ab300-0 <officialabhishek3000@gmail.com>
2024-11-09 22:35:08 +0530

    working on bug post data

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
 import useStyles from './styles';
+import FileBase from 'react-file-base64';
+import { useDispatch } from 'react-redux';
+import { TextField,Button,Typography,Paper } from '@material-ui/core';
+import { createPost } from '../../actions/posts'
 function Form() {
+    const [postData, setPostData] = useState({  creator : '',title : '',message : '',tags:'',selectedFile : ''} );
+
     const classes=useStyles();
+    const dispatch=useDispatch();
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        dispatch(createPost(postData));
+    }
+    const clear = ()=>{
+
+    };
   return (
-    <div>Form</div>
+    <Paper className={classes.paper}>
+        <form autoComplete='Off' noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
+            <Typography variant='h6' >Creating a memory</Typography>
+
+            <TextField name='creator' variant='outlined' label='Creator' fullWidth value={postData.creator} onChange={(e)=>setPostData({...postData, creator:e.target.value})}/>
+
+            <TextField name='title' variant='outlined' label='Title' fullWidth value={postData.title} onChange={(e)=>setPostData({...postData, title:e.target.value})}/>
+
+            <TextField name='message' variant='outlined' label='Message' fullWidth value={postData.message} onChange={(e)=>setPostData({...postData, message:e.target.value})}/>
+
+            <TextField name='tags' variant='outlined' label='Tags' fullWidth value={postData.tags} onChange={(e)=>setPostData({...postData, tags:e.target.value})}/>
+
+          <div className={classes.fileInput}>
+            <FileBase
+              type='file'
+              multiple={false}
+              onDone={(base64)=>setPostData({...postData, selectedFile:base64})}
+            />
+          </div>
+
+          <Button className={classes.buttonSubmit} color='primary' size='large' variant='contained' type='submit' fullWidth>submit</Button>
+          <Button className={classes.buttonSubmit} color='secondary' size='small' variant='contained' onClick={clear} fullWidth>clear</Button>
+        </form>
+    </Paper>
   )
 }
 
--- server/index.js
+++ server/index.js
@@ -5,11 +5,13 @@ import cors from 'cors';
 import postRoutes from './routes/posts.js';
 const app=express();
 
-app.use('/post',postRoutes);
+
 
 app.use(bodyParser.json({limit : "30mb",extended : true}));
 app.use(bodyParser.urlencoded({limit : "30mb",extended : true}));
 app.use(cors());
+
+app.use('/posts',postRoutes); 
 
 const CONNECTION_URL = "mongodb+srv://root:redhat@cluster0.042uihm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const PORT = process.env.PORT || 5000;
